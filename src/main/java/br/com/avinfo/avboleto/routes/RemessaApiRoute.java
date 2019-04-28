a47ad3f2ba4be505f86c82235739082c528bf2ea
tominem@hotmail.com <tominem@hotmail.com>
2019-04-29 00:20:50 +0100

    arrumando serviço de geração de remessa

--- src/main/java/br/com/avinfo/avboleto/routes/RemessaApiRoute.java
+++ src/main/java/br/com/avinfo/avboleto/routes/RemessaApiRoute.java
@@ -1,6 +1,7 @@
 package br.com.avinfo.avboleto.routes;
 
 import static br.com.avinfo.avboleto.sql.Queries.FIND_IDS_INTEGRACAO_BOLETOS_PARA_GERAR_REMESSA;
+import static br.com.avinfo.avboleto.sql.Queries.INSERT_REMESSA;
 import static br.com.avinfo.avboleto.sql.Queries.UPDATE_STATUS_BOLETO_REMESSA;
 
 import java.util.ArrayList;
@@ -11,12 +12,14 @@ import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import br.com.avinfo.avboleto.sql.Queries;
 
 @Component
 public class RemessaApiRoute extends RouteBuilder {
+	
+	@Value(value="${habilitar.https:false}")
+	private Boolean habilitarHttps;
 
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
@@ -59,15 +62,16 @@ public class RemessaApiRoute extends RouteBuilder {
 			.choice()
 				.when(simple("${body} != null && ${body} != ''"))
 					.marshal().json(JsonLibrary.Jackson)
-					.log("AFTER: ${body}")
 					.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
 					.setHeader("cnpj-sh", simple("{{tecnosped.boleto.api.cnpjsh}}"))
 					.setHeader("token-sh", simple("{{tecnosped.boleto.api.tokensh}}"))
 					.setHeader("cnpj-cedente", constant("01001001000113"))
 					.setHeader(Exchange.HTTP_METHOD, HttpMethods.POST)
-					.to("http4://{{tecnosped.boleto.api.host}}/v{{tecnosped.boleto.api.version}}/remessas/lote?throwExceptionOnFailure=false")
+					.log("[api-gerar-remessa]-requisicao-url: {{tecnosped.boleto.api.host}}/v{{tecnosped.boleto.api.version}}/boletos/lote")
+					.log("[api-gerar-remessa]-requisicao-ws-body: ${body}")
+					.toF("%s://{{tecnosped.boleto.api.host}}/v{{tecnosped.boleto.api.version}}/remessas/lote?throwExceptionOnFailure=false", protocol())
 					.unmarshal().json(JsonLibrary.Jackson)
-					.log("RESULT WS: ${body}")
+					.log("[api-gerar-remessa]-retorno-ws-body: ${body}")
 					.to("direct:db-insert-remessa-sucesso")
 //					.to("direct:db-insert-remessa-falha")
 		.end();
@@ -78,7 +82,7 @@ public class RemessaApiRoute extends RouteBuilder {
 			.choice()
 				.when(simple("${body[_status]} == 'sucesso'"))
 					.split().simple("${body[_dados][_sucesso]}")
-						.to("sql:" + Queries.INSERT_REMESSA + "?dataSourceRef=dataSource")
+						.to("sql:" + INSERT_REMESSA + "?dataSourceRef=dataSource")
 						.setHeader("ids_integracao", simple("${body[titulos]}"))
 						.process(ex -> {
 							
@@ -101,6 +105,10 @@ public class RemessaApiRoute extends RouteBuilder {
 						.to("sql:" + UPDATE_STATUS_BOLETO_REMESSA + "?dataSourceRef=dataSource")
 		.end();
 		
+	}
+	
+	private String protocol() {
+		return habilitarHttps ? "https4" : "http4";
 	}
 
 }
